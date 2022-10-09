c456e9669c2ed8ea67fc8725805eb00103ce98b1
Mseong <mseong123@hotmail.com>
2022-10-09 12:41:45 +0800

    try again

--- utilities/db.js
+++ utilities/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 const moment = require('moment-timezone');
-const dateBangkok = moment.tz(Date.now(), "Asia/Bangkok");
+
 
 const chatSchema=new mongoose.Schema({
     _id:String,
@@ -12,7 +12,7 @@ const chatSchema=new mongoose.Schema({
         },
         time:{
             type:Date,
-            default:dateBangkok
+            default:Date.now
         }
         }],
     }
