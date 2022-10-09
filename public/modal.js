2566912abf2a99fa988b96467d5c2ee46748f8e3
Mseong <mseong123@hotmail.com>
2022-10-09 13:14:24 +0800

    changed date rendering at server

--- public/modal.js
+++ public/modal.js
@@ -1,7 +1,7 @@
 function pug_attr(t,e,n,r){if(!1===e||null==e||!e&&("class"===t||"style"===t))return"";if(!0===e)return" "+(r?t:t+'="'+t+'"');var f=typeof e;return"object"!==f&&"function"!==f||"function"!=typeof e.toJSON||(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||-1===e.indexOf('"'))?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"}
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
 var pug_match_html=/["&<>]/;
-function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function modalTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {".\\views\\component\u002Fmodal.pug":"each value in private\r\n    div(id='chat'+value._id tabindex='-1').modal.fade\r\n        .modal-dialog \r\n            .modal-content.modal-content-custom\r\n                .modal-header.modal-header-custom\r\n                    h3#header-nickname \r\n                        span(style='vertical-align:middle').fa.fa-lg.fa-user-circle\r\n                        |\r\n                        |\r\n                        |\r\n                        .\r\n                            #{value.nickname}\r\n                    button(type='button' data-dismiss='modal').close.close-custom\r\n                        span.fa.fa-close.fa-close-custom\r\n                .modal-body.modal-body-custom\r\n                    - var chat=value.chat\r\n                    each value in chat\r\n                        if value.self\r\n                            include card-self.pug \r\n                              \r\n                        else\r\n                            include card-not-self.pug\r\n                            \r\n                                            \r\n                .modal-footer.d-block\r\n                    form(onsubmit=\"formOnSubmit(event)\" id='form'+value._id)\r\n                        .form-row\r\n                            .col-1 \r\n                                button.btn.btn-light\r\n                                    span.fa.fa-plus\r\n                            .col-9\r\n                                input(type='text' id='input'+value._id required).form-control\r\n                            .col\r\n                                button(type='submit' onclick='formOnClick(event)' id='submit'+value._id data-socket=value.socketID).btn.btn-dark\r\n                                    span.fa.fa-arrow-right","views\\component\\card-self.pug":".d-flex.justify-content-end\r\n    .card.card-custom-1\r\n        .card-body.d-flex.card-body-custom\r\n            p.mb-0.mr-auto #{value.text}\r\n            p.mb-0 \r\n                | &nbsp;\r\n                | &nbsp;\r\n                | &nbsp;\r\n                - var date= new Date(value.time)\r\n            p.mb-0.text-nowrap\r\n                span.small  #{date.getHours()+':'+String(date.getMinutes()).padStart(2, '0')}\r\n                | &nbsp;\r\n                span.small-custom #{date.getDate()+' '+date.toLocaleString('default', { month: 'long' }).substring(0,3)+' '+date.getFullYear().toString().substr(-2)}      ","views\\component\\card-not-self.pug":".d-flex.justify-content-start\r\n    .card.card-custom-2\r\n        .card-body.d-flex.card-body-custom\r\n            p.mb-0.mr-auto #{value.text}\r\n            p \r\n                | &nbsp;\r\n                | &nbsp;\r\n                | &nbsp;\r\n                - var date= new Date(value.time)\r\n            p.mb-0.text-nowrap\r\n                span.small  #{date.getHours()+':'+String(date.getMinutes()).padStart(2, '0')}\r\n                | &nbsp;\r\n                span.small-custom #{date.getDate()+' '+date.toLocaleString('default', { month: 'long' }).substring(0,3)+' '+date.getFullYear().toString().substr(-2)}   "};
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function modalTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {".\\views\\component\u002Fmodal.pug":"each value in private\r\n    div(id='chat'+value._id tabindex='-1').modal.fade\r\n        .modal-dialog \r\n            .modal-content.modal-content-custom\r\n                .modal-header.modal-header-custom\r\n                    h3#header-nickname \r\n                        span(style='vertical-align:middle').fa.fa-lg.fa-user-circle\r\n                        |\r\n                        |\r\n                        |\r\n                        .\r\n                            #{value.nickname}\r\n                    button(type='button' data-dismiss='modal').close.close-custom\r\n                        span.fa.fa-close.fa-close-custom\r\n                .modal-body.modal-body-custom\r\n                    - var chat=value.chat\r\n                    each value in chat\r\n                        if value.self\r\n                            include card-self.pug \r\n                              \r\n                        else\r\n                            include card-not-self.pug\r\n                            \r\n                                            \r\n                .modal-footer.d-block\r\n                    form(onsubmit=\"formOnSubmit(event)\" id='form'+value._id)\r\n                        .form-row\r\n                            .col-1 \r\n                                button.btn.btn-light\r\n                                    span.fa.fa-plus\r\n                            .col-9\r\n                                input(type='text' id='input'+value._id required).form-control\r\n                            .col\r\n                                button(type='submit' onclick='formOnClick(event)' id='submit'+value._id data-socket=value.socketID).btn.btn-dark\r\n                                    span.fa.fa-arrow-right","views\\component\\card-self.pug":".d-flex.justify-content-end\r\n    .card.card-custom-1\r\n        .card-body.d-flex.card-body-custom\r\n            p.mb-0.mr-auto #{value.text}\r\n            p.mb-0 \r\n                | &nbsp;\r\n                | &nbsp;\r\n                | &nbsp;\r\n                - var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia\u002FSingapore'}))\r\n            p.mb-0.text-nowrap\r\n                span.small  #{date.getHours()+':'+String(date.getMinutes()).padStart(2, '0')}\r\n                | &nbsp;\r\n                span.small-custom #{date.getDate()+' '+date.toLocaleString('default', { month: 'long' }).substring(0,3)+' '+date.getFullYear().toString().substr(-2)}      ","views\\component\\card-not-self.pug":".d-flex.justify-content-start\r\n    .card.card-custom-2\r\n        .card-body.d-flex.card-body-custom\r\n            p.mb-0.mr-auto #{value.text}\r\n            p \r\n                | &nbsp;\r\n                | &nbsp;\r\n                | &nbsp;\r\n                - var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia\u002FSingapore'}))\r\n            p.mb-0.text-nowrap\r\n                span.small  #{date.getHours()+':'+String(date.getMinutes()).padStart(2, '0')}\r\n                | &nbsp;\r\n                span.small-custom #{date.getDate()+' '+date.toLocaleString('default', { month: 'long' }).substring(0,3)+' '+date.getFullYear().toString().substr(-2)}   "};
 ;var locals_for_with = (locals || {});(function (Date, String, private) {;pug_debug_line = 1;pug_debug_filename = ".\\views\\component\u002Fmodal.pug";
 // iterate private
 ;(function(){
@@ -79,7 +79,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -124,7 +124,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-not-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -176,7 +176,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -221,7 +221,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-not-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -345,7 +345,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -390,7 +390,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-not-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -442,7 +442,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
@@ -487,7 +487,7 @@ pug_html = pug_html + "\n";
 ;pug_debug_line = 8;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "&nbsp;";
 ;pug_debug_line = 9;pug_debug_filename = "views\\component\\card-not-self.pug";
-var date= new Date(value.time)
+var date= new Date(new Date(value.time).toLocaleString('en-US', { timeZone: 'Asia/Singapore'}))
 pug_html = pug_html + "\u003C\u002Fp\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "views\\component\\card-not-self.pug";
 pug_html = pug_html + "\u003Cp class=\"mb-0 text-nowrap\"\u003E";
